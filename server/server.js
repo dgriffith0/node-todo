fadfb2b642406dbecc481c3ff9c2bf5c445ca19c
Dan Griffith <dan.griffith@tierpoint.com>
2018-03-27 14:44:14 -0500

    patch route to complete todo

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ require('./config/config');
 var {mongoose, ObjectID} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const express = require('express');
 
-var bodyParser = require('body-parser');
-var express = require('express');
-
-const app = express();
+var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -58,6 +58,38 @@ app.delete('/todos/:id', (req, res) => {
         }
         res.status(200).send(JSON.stringify({deleted: {todo}}));
     }).catch((err) => res.status(400).send(err));
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } else {
+            res.send({todo});
+        }
+    });
+});
+
+app.post('/users', (req, res) => {
+    var user = new User({
+        text: req.body.text
+    }).save().then((doc) => {
+        res.status(200).send(doc);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.get('/', (req, res) => {
